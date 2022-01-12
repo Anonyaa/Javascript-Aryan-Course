5bf17761464133cffff1339536a113358737cdf0
Anonyaa <ananyak@khanlabschool.org>
2022-01-12 09:58:40 -0800

    Update .js

--- .js
+++ .js
@@ -1,1 +1,1 @@
-console.log("hello world")
+console.log("I am Ananya. I have a little experience in Javascript and Python, and I am excited to start learning more Javascript and getting more used to it.")
