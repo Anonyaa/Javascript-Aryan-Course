b8057edbf6f4d27e67f163ae5472bdc54b507914
Anonyaa <ananyak@khanlabschool.org>
2022-01-14 23:15:53 -0800

    Update mathcalculator.js

--- assignments/mathcalculator.js
+++ assignments/mathcalculator.js
@@ -1,24 +1,22 @@
+//basic structure of calculator: 
+//user input: 
 let prompt = require('prompt-sync')();
 let value1 =  prompt("Give me your first number: ");
 let value2 = prompt("And give me your second number: ");
 let operation = prompt("And what operation do you need? ")
-
+//strings to integers
 let x = parseInt(value1);
 
 let y = parseInt(value2);
-
+//if-else statements 
 if (operation == "addition") {
-    console.log(x,"+", y, "=")
-    console.log(x + y)
+    console.log(x,"+", y, "=", (x+y))
 } else if (operation == "subtraction") {
-    console.log(x,"-", y, "=")
-    console.log(x - y)
+    console.log(x,"-", y, "=", (x-y))
 } else if (operation == "multiplication") {
-    console.log(x,"*", y, "=")
-    console.log(x * y)
+    console.log(x,"*", y, "=", (x*y))
 } else if (operation == "division") {
-    console.log(x,"/", y, "=")
-    console.log(x / y)
+    console.log(x,"/", y, "=", (x/y))
 } else {
     console.log("I don't know what that means. Please write either addition, subtraction, multiplication, or division.")
 }
